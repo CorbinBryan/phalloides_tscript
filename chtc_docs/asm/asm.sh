07ff411564c20b41a120cc93df70602b7a04dcfd
CorbinBryan <bryan7@wisc.edu>
2025-10-23 10:53:20 -0500

    updated asm.sh

--- chtc_docs/asm/asm.sh
+++ chtc_docs/asm/asm.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 # HTCondor's job manager passes arguments as positional inputs; $1 is the first argument. 
 ACCESS=$1
-
+# Copy over input files over 200 Mb
 cp /staging/bryan7/raw_data/201016_AHL2WCDSXY/${ACCESS}_R1_001.fastq.gz ./
 cp /staging/bryan7/raw_data/201016_AHL2WCDSXY/${ACCESS}_R2_001.fastq.gz ./
 
-
+# Inflate compressed files (files compressed previous to decrease storage requirements)
 gunzip ${ACCESS}_R1_001.fastq.gz
 gunzip ${ACCESS}_R2_001.fastq.gz
-
+# make directory with the name of our transcriptome; move all inputs into our directory
 mkdir ./${ACCESS}
 
 mv ${ACCESS}*.fastq ./${ACCESS}
 
 cd ./${ACCESS}
-
+# run fastqc to check quality on both forward and reverse reads
 /root/FastQC/fastqc ${ACCESS}*
-
+# generate a fasta file containing the illumina adapter oligios. This could be transfered at runtime too. 
 echo -e ">Illumina_universal_adaptor\nAATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT" > adapters.fasta
-
+# Run trimmomatic 
 java -jar /root/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 4 -phred33\
  ${ACCESS}_R1_001.fastq ${ACCESS}_R2_001.fastq\
  ${ACCESS}_P1.fastq ${ACCESS}U1.fastq ${ACCESS}_P2.fastq ${ACCESS}U2.fastq\
@@ -27,11 +27,11 @@ java -jar /root/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 4 -phred33\
  TRAILING:3\
  SLIDINGWINDOW:4:15\
  MINLEN:150
-
+# run spades
 spades.py -1 ${ACCESS}_P1.fastq -2 ${ACCESS}_P1.fastq -o ./assembly --cov-cutoff auto --rna 
-
-rm *fastq*
-
+# remove raw reads
+rm *fastq
+# compress outputs at transfer to staging. 
 cd .. 
 
 tar -czf ${ACCESS}.tar.gz ./${ACCESS}
